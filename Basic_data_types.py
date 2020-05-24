2fccb36fae9f8440ce7d8cd9b303ae157c502c19
michaelrizzo2 <m_rizzo@live.com>
2020-05-24 07:59:53 -0400

    Got up to boolean values.

--- Basic_data_types.py
+++ Basic_data_types.py
@@ -2,3 +2,21 @@
 #str,int,boolean value, and float ar the basic data types.
 
 data_type=type(12)
+
+print (f"Data type is {data_type}")
+
+data_type_two=type(12.08)
+
+print (f"Data type is {data_type_two}")
+
+data_type_three=type("this ia a string")
+
+print (f"Data type is {data_type_three}")
+
+boolean_value=True
+
+boolean_value_other=False
+
+print(type(boolean_value),type(boolean_value_other))
+
+print(f"first boolean value is {boolean_value} and the second boolean value is {boolean_value_other}")
