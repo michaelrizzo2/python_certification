86a081bec3ab2cf7a485eba88840222d1e8c5eb4
michaelrizzo2 <m_rizzo@live.com>
2020-05-24 11:29:23 -0400

    finished the leson.

--- basic_arithmetic.py
+++ basic_arithmetic.py
@@ -6,8 +6,19 @@ number_2=10
 
 my_divison_answer=number_1/number_2 #This does float division in Python 3 and integer division in Python 2
 
-my_multiplication_answer=number_1*number_2 #This does float division in Python 3 and integer division in Python 2
+my_multiplication_answer=number_1*number_2 
+
+my_subtraction_number=number_1-number_2 
+
+my_remainder_answer=number_1%number_2
 
 print(f"my division answer is {my_divison_answer}")
 
 print(f"my multiplication answer is {my_multiplication_answer}")
+
+print(f"my subtraction answer is {my_subtraction_number}")
+
+print(f"my modulus(remainder) answer is {my_remainder_answer}")
+
+
+
