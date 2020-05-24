156a1176fd73e315ab34241132c1729711cdf185
michaelrizzo2 <m_rizzo@live.com>
2020-05-23 23:53:24 -0400

    Basics of variables

---
+++ variables.py
@@ -0,0 +1,15 @@
+print ("Hello world")
+
+print("This is the next statement")
+
+number=3468
+
+print('number')#This will print ou the string number and not the actual value
+
+print (number)
+
+weight=23
+
+my_answer=number+weight
+
+print(my_answer)
