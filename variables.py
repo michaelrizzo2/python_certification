b297fa472d17fd3fe8ac697d9deaa9ae42985cc0
michaelrizzo2 <m_rizzo@live.com>
2020-05-24 07:40:19 -0400

    Working on Variables.

--- variables.py
+++ variables.py
@@ -13,3 +13,20 @@ weight=23
 my_answer=number+weight
 
 print(my_answer)
+
+#my_name="Michael Rizzo"
+
+first_name="Michael"
+last_name="Rizzo"
+
+my_name=first_name+last_name
+print (my_name)
+
+my_other_name=first_name+" "+last_name
+print (my_other_name)
+
+sentence=first_name+last_name + " weighs 100 lbs"
+print (sentence)
+
+other_sentence=first_name+last_name + " weighs "+ str(weight) +" lbs"
+print (sentence)
