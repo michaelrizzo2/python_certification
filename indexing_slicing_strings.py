ff7ecf9f3d79a6f5b4603e72e5d6345577257509
michaelrizzo2 <m_rizzo@live.com>
2020-05-24 12:03:03 -0400

    Next step in the slicing process.

--- indexing_slicing_strings.py
+++ indexing_slicing_strings.py
@@ -2,6 +2,16 @@ sentence="i'm coming home"
 
 new_sentence="i'm coming \n home"
 
+next_sentence="This is my new sentence"
+
 print(sentence)
 
 print(new_sentence)
+
+#We are now going to print the first character
+
+#The first index will start at zero.
+
+print(f"The first character is {next_sentence[0]}")
+
+#The first inedx will start at zero and will increase  by one
