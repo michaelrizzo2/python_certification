da89b77468e7ae6d75c0258da6f775aebcdf8b2e
michaelrizzo2 <m_rizzo@live.com>
2020-05-24 11:19:03 -0400

    beginning of basic arithmetic

---
+++ basic_arithmetic.py
@@ -0,0 +1,9 @@
+#This wil show basic arithmetic in Python 3
+
+number_1=3
+
+number_2=10
+
+my_answer=number_1/number_2 #This does float division in Python 3 and intereg division in Python 2
+
+print(f"my answer is {my_answer}")
