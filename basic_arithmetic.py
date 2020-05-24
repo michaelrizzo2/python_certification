c6928829f990d8df57a32fb034afbc063f686f34
michaelrizzo2 <m_rizzo@live.com>
2020-05-24 11:21:08 -0400

    added multiplication.

--- basic_arithmetic.py
+++ basic_arithmetic.py
@@ -4,6 +4,10 @@ number_1=3
 
 number_2=10
 
-my_answer=number_1/number_2 #This does float division in Python 3 and intereg division in Python 2
+my_divison_answer=number_1/number_2 #This does float division in Python 3 and integer division in Python 2
 
-print(f"my answer is {my_answer}")
+my_multiplication_answer=number_1*number_2 #This does float division in Python 3 and integer division in Python 2
+
+print(f"my division answer is {my_divison_answer}")
+
+print(f"my multiplication answer is {my_multiplication_answer}")
