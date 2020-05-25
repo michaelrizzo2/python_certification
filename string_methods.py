ab5f30a6636124c8f244a7eeb14595a88937ef91
michaelrizzo2 <m_rizzo@live.com>
2020-05-24 22:55:27 -0400

    Used upper and lower case methods.

---
+++ string_methods.py
@@ -0,0 +1,8 @@
+#This will show all of the various string methods
+my_sentence="This is my test sentence"
+
+#We will make the whole sentence uppercase
+print(f"{my_sentence.upper()}")
+
+#we can make the whole sentence lowercase
+print(f"{my_sentence.lower()}")
