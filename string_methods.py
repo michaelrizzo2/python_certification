3a0d5c8a34e30af2dd07fd168b117667cfb04a63
michaelrizzo2 <m_rizzo@live.com>
2020-05-24 23:09:55 -0400

    Showed more method for strings

--- string_methods.py
+++ string_methods.py
@@ -6,3 +6,15 @@ print(f"{my_sentence.upper()}")
 
 #we can make the whole sentence lowercase
 print(f"{my_sentence.lower()}")
+
+#we can Capitalize the first letter of each word
+print(f"{my_sentence.capitalize()}")
+
+#isdigit will return true if the string is a number
+print(f'{my_sentence.isdigit()}')
+
+#isalnum will return if a string has numbers or letters it does not recognize spaces.
+print(f"{my_sentence.isalnum()}")
+
+#startswith method will check to see if a method starts with a set of characters\
+print(f"{my_sentence.startswith("A948")}")
