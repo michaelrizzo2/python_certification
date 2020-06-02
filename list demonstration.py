a5d9e10e027dfc399eaae58209daafa3bfa26ae3
michaelrizzo2 <m_rizzo@live.com>
2020-06-01 20:58:10 -0400

    showed the resultys of the pop method.

--- list demonstration.py
+++ list demonstration.py
@@ -6,3 +6,7 @@ print (f" my list is {my_list}")
 
 #NOw we are going to show the type of the list
 print (f"The type of the list is {type(my_list)}")
+
+#We are going to show the pop method and what it does to a list
+print (f"The result is {my_list.pop()}")# pop will remove the last element from a list 
+
