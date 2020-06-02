fc739dfcd5290a1ff4f892e4920a0ca200f42857
michaelrizzo2 <m_rizzo@live.com>
2020-06-01 20:54:42 -0400

    showed the type of the list is a list

--- list demonstration.py
+++ list demonstration.py
@@ -3,3 +3,6 @@ my_list=[1,2,3,4,5]
 #Now we will print the contents of my list
 
 print (f" my list is {my_list}")
+
+#NOw we are going to show the type of the list
+print (f"The type of the list is {type(my_list)}")
