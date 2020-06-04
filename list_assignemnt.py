303fc680a79ad974883ab9fcbab9de74544239e4
michaelrizzo2 <m_rizzo@live.com>
2020-06-03 21:47:28 -0400

    stuck on sorting list

--- list_assignemnt.py
+++ list_assignemnt.py
@@ -4,4 +4,4 @@ list_2=[1,2,3,4,5]
 #we want to sort each list in reverse and then get the last 3 entries.
 #list_2.sort().reverse()[-3:-1]
 #list_1.sort().reverse()[-3:-1]
-
+print(list_1.reverse())
