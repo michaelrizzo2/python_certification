977a980eb814cc198f22bec3cff4347957a3cb9f
michaelrizzo2 <m_rizzo@live.com>
2020-06-03 21:59:56 -0400

    Assignment complete

--- list_assignemnt.py
+++ list_assignemnt.py
@@ -4,4 +4,11 @@ list_2=[1,2,3,4,5]
 #we want to sort each list in reverse and then get the last 3 entries.
 #list_2.sort().reverse()[-3:-1]
 #list_1.sort().reverse()[-3:-1]
-print(list_1.reverse())
+list_1.sort()
+list_1.reverse()
+
+list_2.sort()
+list_2.reverse()
+
+print(list_1[2:]+list_2[2:])
+#['d', 'b', 'a', 3, 2, 1]
