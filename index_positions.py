f265e473881fb08515fb543e87f705441135a127
michaelrizzo2 <m_rizzo@live.com>
2020-06-05 19:06:52 -0400

    Showed how the count method worked for lists.

--- index_positions.py
+++ index_positions.py
@@ -1,3 +1,7 @@
-my_list=['a','b','c','d']
+my_list=['a','b','c','d','c','c']
 my_list_index=my_list.index('c')
 print (f"The index of c is {my_list_index}")
+
+#We will show the count method
+c_count=my_list.count('c')
+print(f"The number of c's is {c_count}")
