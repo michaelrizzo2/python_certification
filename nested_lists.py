a931e72bb241f1fcf324df06c3af47af89396750
michaelrizzo2 <m_rizzo@live.com>
2020-06-05 12:40:54 -0400

    Simple entry retrieval example done.

--- nested_lists.py
+++ nested_lists.py
@@ -1,1 +1,7 @@
 my_list=['a','b','c',[1,2,3],'d']
+
+#We need to get the list inside of the list
+#my_list[3] the list is the fourth entry
+#My list 2 will give us the 3rd entry inside the previous entry 
+#we want to get 3 from the nested list
+print (f"the value we want is {my_list[3][2]}")
