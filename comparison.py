2857506de1ce0439566c66bec214d8c8effbf3cd
michaelrizzo2 <m_rizzo@live.com>
2020-06-10 18:32:35 -0400

    Did greater than and less than.

--- comparison.py
+++ comparison.py
@@ -9,4 +9,7 @@ print("Hello"=="hello")
 #Srting and integer comparison
 print(10=='10')# a comparison operator also looks at the type of the data along with the quantity
 #5 will be equal to 5.00 but not 5.01
-#Comment
+#We will show the greater than and less than operator
+
+print(5<10)#This will be true
+print(5>10)#This will be false 
