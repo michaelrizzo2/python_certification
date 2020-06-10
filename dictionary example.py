a4a862799595a1b5d066f85e7c7b33ab757f641b
michaelrizzo2 <m_rizzo@live.com>
2020-06-09 21:24:16 -0400

    Dictionary was printed

--- dictionary example.py
+++ dictionary example.py
@@ -9,4 +9,5 @@ print(my_dict[7])
 my_dict[7]="ivbiebvav"
 print(my_dict[7])
 
-people_weight={"rastachu",34,"Regular pikachu",17,"Ash" : 140}
+people_weight={"rastachu":34,"Regular pikachu":17,"Ash":140}
+print(people_weight)
