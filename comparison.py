2dc2199e828ed371aeb0895a53e64ee347517fa2
michaelrizzo2 <m_rizzo@live.com>
2020-06-10 16:35:46 -0400

    String ands integer comparison

--- comparison.py
+++ comparison.py
@@ -1,3 +1,10 @@
 #A comparison operator will return true or false
 print(10==10)
 print(10!=10)
+
+#String comparison
+print("Hello"=="Hello")
+print("Hello"=="hello")
+
+#Srting and integer comparison
+print(10==10)
