41aebac07c2d2cd7b84fea2c4314ec9e08ce9cc3
michaelrizzo2 <m_rizzo@live.com>
2020-06-09 21:12:06 -0400

    Printed value from a key

--- dictionary example.py
+++ dictionary example.py
@@ -1,3 +1,5 @@
 #The basic of a dictionary is a key is linked to a value
-my_dict={"k1","My data"}
+my_dict={"k1":"My data"}
 #Dictionaries are key oriented not index oriented
+#Lists and tuples are index oriented
+print(my_dict["k1"])
