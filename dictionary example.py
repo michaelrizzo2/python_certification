3f3b163d98b72b665fba1a81c8b9d37620708899
michaelrizzo2 <m_rizzo@live.com>
2020-06-09 21:15:55 -0400

    Chnaged data in a dictionary

--- dictionary example.py
+++ dictionary example.py
@@ -1,5 +1,10 @@
 #The basic of a dictionary is a key is linked to a value
-my_dict={"k1":"My data"}
+my_dict={"k1":"My data",7:"other data"}
 #Dictionaries are key oriented not index oriented
 #Lists and tuples are index oriented
 print(my_dict["k1"])
+
+print(my_dict[7])
+#you cna change a dictionary unlike a tuple
+my_dict[7]="ivbiebvav"
+print(my_dict[7])
