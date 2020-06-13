51387677d8e683d6a76b4b73f58c7ffae9c9e976
michaelrizzo2 <m_rizzo@live.com>
2020-06-12 22:23:58 -0400

    Finshed the basics of functions

--- Section_02/function_basics.py
+++ Section_02/function_basics.py
@@ -8,11 +8,21 @@ def greet_person(name="Name Here"):
     The only input is the name
     '''
     print("This is the body of a function")
-    print("Hello this is a greeting from "+name)
+    return "Hello this is a greeting from "+name
 
 #Do not forget to call the function
-greet_person()
-greet_person("michael")
+greeting1=greet_person()
+greeting2=greet_person("michael")
 #we need to define the function before we call the function,otherwise we will get an error saying the function does not exist 
 #you can also set a default value for the arguement
 #to convert a interger to a string do str(variable)
+# we add return to a function so we can use the function for multiple things without having to rewrite the function each time.
+print(f"The first result is {greeting1} and the second result is {greeting2}")
+
+def remainder(number1,number2):
+    result=number1% number2
+    return result
+out=remainder(3,2)
+print(f" The result is {out}")
+
+#Python runs code from the top to the bottom and the indentation needs to be consistent(do not mix tabs and spaces.)
