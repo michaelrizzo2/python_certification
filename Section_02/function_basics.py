3babbb93da9a72aec36e14a0bb24885079666532
michaelrizzo2 <m_rizzo@live.com>
2020-06-12 22:04:01 -0400

    Added arguements to a function

--- Section_02/function_basics.py
+++ Section_02/function_basics.py
@@ -2,9 +2,9 @@
 print("Hello World")#"Hello world is the arguement"
 
 #To define a function we use the def command
-def greet_person():
-    print("Hello this is a greeting")
+def greet_person(name):
+    print("Hello this is a greeting from "+name)
 
 #Do not forget to call the function
-greet_person()
+greet_person("michael")
 #we need to define the function before we call the function,otherwise we will get an error saying the function does not exist 
