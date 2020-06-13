fbb5c704fa54e93f2c5d741afe17869d3b5c382f
michaelrizzo2 <m_rizzo@live.com>
2020-06-12 21:49:52 -0400

    Set a function and called it

--- Section_02/function_basics.py
+++ Section_02/function_basics.py
@@ -2,3 +2,8 @@
 print("Hello World")#"Hello world is the arguement"
 
 #To define a function we use the def command
+def greet_person():
+    print("Hello this is a greeting")
+
+#Do not forget to call the function
+greet_person()
