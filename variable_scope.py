aa03f57673c36a406511ffdc75db5db2fc9364e9
michaelrizzo2 <m_rizzo@live.com>
2020-06-15 16:55:17 -0400

    Finished Variable Scope.

---
+++ variable_scope.py
@@ -0,0 +1,10 @@
+#we are going to discuss variable scope in this lesson
+age=27#This age is a global variable
+
+def increase_age():
+    age=30#This age is a local variable.
+    return age
+
+print(age)#This will print the 27 due to the fact that we do not have access to the function right now
+
+print(increase_age())#This will give us 30 because we are accessing the age inside the function. 
