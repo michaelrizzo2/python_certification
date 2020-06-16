f28e96d4b03782ab5be09490d850de82b703b17b
michaelrizzo2 <m_rizzo@live.com>
2020-06-16 15:47:53 -0400

    Working with nested functions.

--- variable_scope.py
+++ variable_scope.py
@@ -3,8 +3,11 @@ age=27#This age is a global variable
 
 def increase_age():
     age=30#This age is a local variable.
-    return age
-
+    def add_4_to_age(age):
+        age+=4
+        print(f"Nested Method and the age is {age}")
+    add_4_to_age(age)
+    print(age)#This will print 30 because the only age we have is 30
 print(age)#This will print the 27 due to the fact that we do not have access to the function right now
 
 print(increase_age())#This will give us 30 because we are accessing the age inside the function. 
