749e8670a967a9d064d64b54c5a77253b759ebd0
michaelrizzo2 <m_rizzo@live.com>
2020-06-15 20:08:57 -0400

    Continued with scope and nested functions.

--- variable_scope.py
+++ variable_scope.py
@@ -8,3 +8,5 @@ def increase_age():
 print(age)#This will print the 27 due to the fact that we do not have access to the function right now
 
 print(increase_age())#This will give us 30 because we are accessing the age inside the function. 
+
+#We are going to continue with scope and nested functions
