abc1cd0791c328b124f0a2df5df7606c17c323db
michaelrizzo2 <m_rizzo@live.com>
2020-06-17 22:08:18 -0400

    Added two conditions to the if statement.

--- if_statements.py
+++ if_statements.py
@@ -4,7 +4,7 @@ value= 7<6
 other_value=7>6
 print(other_value,value)
 #This proves that if and else rely on boolean logic
-if 5<6:
+if (5<6 and other_value==True):
     print(" 5 is less than 6")
 else:
     print(" 5 is not less than 6")
