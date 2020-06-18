ea52d3e9736859d77f00e300643f06bb0a743804
michaelrizzo2 <m_rizzo@live.com>
2020-06-18 11:13:14 -0400

    Iterated through a string character by character.

--- for_loops.py
+++ for_loops.py
@@ -1,4 +1,16 @@
 #we are going to do iteration in python
 farm_animals=["Goat","Horse","Pig","Sheep","Duck","Cow"]
+#for animal in farm_animals:
+    #print (f"The animal is {animal}")
+    #print(f"The {animal} is safe in the farm.")
+
+counter=0
+
 for animal in farm_animals:
-    print (f"The animal is {animal}")
+    counter+=1
+    print(f"{counter} {animal} is safe in our farm")
+
+greeting="Hello My name is Michael"
+
+for char in greeting:#This will iterate character by character
+    print(f"The character is {char}")
