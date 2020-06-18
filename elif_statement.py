53a1ad19823fd018ed481d6baa71c6030cc23e34
michaelrizzo2 <m_rizzo@live.com>
2020-06-17 22:27:16 -0400

    Finished elif statement.

---
+++ elif_statement.py
@@ -0,0 +1,10 @@
+animal="monkey"
+
+if animal=="cow":
+    print("The animal is a cow")
+elif animal=="pig":
+    print("The animal is a pig")
+elif (animal=="monkey" or animal=="ape"):
+    print("The animal is a monkey or an ape.")
+else:
+    print("This is getting weird")
