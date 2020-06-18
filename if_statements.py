02aadae4379824b626b154e90ebf4bd718b1888b
michaelrizzo2 <m_rizzo@live.com>
2020-06-17 21:59:55 -0400

    Showed if conditions are based on boolean logic.

--- if_statements.py
+++ if_statements.py
@@ -1,5 +1,9 @@
 x=7
-if x<6:
-    print(" 5 is less than 6")
-else:
-    print(" 5 is not less than 6")
+#if x<6:
+value= 7<6
+other_value=7>6
+print(other_value,value)
+#This poroves that if and else rely on boolean logic
+#    print(" 5 is less than 6")
+#else:
+#    print(" 5 is not less than 6")
