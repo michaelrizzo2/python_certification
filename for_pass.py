d407ecd851599359d199a985736727eba03532e3
michaelrizzo2 <m_rizzo@live.com>
2020-06-18 14:16:17 -0400

    Put pass inside a for lopa and did other things.

--- for_pass.py
+++ for_pass.py
@@ -1,1 +1,10 @@
 #we are going to be shoing the pass keyword.
+my_list=["car","computer"]
+
+for item in my_list:
+    #If we want to work on the for loop later we have to put the pass keyword
+    pass
+
+#do other things
+
+print("Hi")
