ad6af9aaec6b0030667eef7ac8e9182c159d1104
michaelrizzo2 <m_rizzo@live.com>
2020-06-17 22:04:54 -0400

    Showed if true block.

--- if_statements.py
+++ if_statements.py
@@ -4,6 +4,10 @@ value= 7<6
 other_value=7>6
 print(other_value,value)
 #This proves that if and else rely on boolean logic
-#    print(" 5 is less than 6")
-#else:
-#    print(" 5 is not less than 6")
+if 5<6:
+    print(" 5 is less than 6")
+else:
+    print(" 5 is not less than 6")
+#if true blocks will always run 
+if True:
+    print("This will always run")
