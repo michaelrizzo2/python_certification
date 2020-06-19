934be01ca5c48fe223e78b010b94cff344763ca9
michaelrizzo2 <m_rizzo@live.com>
2020-06-18 21:53:09 -0400

    Printed the keys values and items for a dictionary.

---
+++ tuple_looping.py
@@ -0,0 +1,9 @@
+employees={"mike":77000,"Robert":100000,"Pikachu":40000}
+for key,value in employees.items():
+    print(key,value)
+
+for value in employees.values():
+    print(value)
+
+for value in employees.keys():
+    print(value)
