d85d13385a9d0fb56e7af875248cd415c4f5edfa
michaelrizzo2 <m_rizzo@live.com>
2020-06-19 23:53:17 -0400

    We will do the zip function tomorrow.

--- range_enumerate.py
+++ range_enumerate.py
@@ -1,6 +1,8 @@
 number_list=[]
 
-for number in range(0,6):#The last number in the range is not inclusive.
+for number in range(1,6,2):#The last number in the range is not inclusive.
     number_list.append(number)
 
 print(number_list)
+
+#Now we wil show off the zip function.
