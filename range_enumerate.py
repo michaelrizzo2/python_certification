0404efbdd1f37f459bac9bc9fde4f67364d17e1c
michaelrizzo2 <m_rizzo@live.com>
2020-06-22 19:24:32 -0400

    Finished enumerate.

--- range_enumerate.py
+++ range_enumerate.py
@@ -15,7 +15,13 @@ for item in out:
     print(f"The item is {item}")
 new_list=[]
 for item_1,item_2 in zip(list_1,list_2):
-    new_list.append(item_1)
+    new_list.append(item_1)#append will always only take one arguement.
     new_list.append(item_2)
 
 print (new_list)    
+#we wil show off the enumerate function.
+for index,entry in enumerate(list_2):
+    print(index,entry)
+#Enumerate will generate a index along with the value automatically.
+
+#The zip function will do the highet nmber of elements that are in each list. If you have a list of 3 entries and a list of 5 entries. The zip function will do only 3 entries.
