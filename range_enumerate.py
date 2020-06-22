f60ca56e70b53356f55a78d094a09cfc8ff723dd
michaelrizzo2 <m_rizzo@live.com>
2020-06-22 19:04:04 -0400

    Shopwed the zip function.

--- range_enumerate.py
+++ range_enumerate.py
@@ -6,3 +6,10 @@ for number in range(1,6,2):#The last number in the range is not inclusive.
 print(number_list)
 
 #Now we wil show off the zip function.
+list_1=[1,2,3,4,5]
+list_2=["My","name","is","Michael","rizzo"]
+out=zip(list_1,list_2)#This will give us a collection of tuples.
+
+
+for item in out:
+    print(f"The item is {item}")
