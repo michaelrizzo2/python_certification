9a5ff3fee25498ea68aedda5ff9fe3e220f3f59d
michaelrizzo2 <m_rizzo@live.com>
2020-06-25 17:05:08 -0400

    Finished Assignment 8.

--- Section_04/assignment_08.py
+++ Section_04/assignment_08.py
@@ -13,3 +13,19 @@ sum78([1, 2, 2, 7, 99, 99, 8]) → 5
 sum78([1, 1, 7, 8, 2]) → 4
 
 """
+def sum78(my_list):
+    my_sum=0
+    continue_sum=True
+    for number in my_list:
+        if number==7:
+            continue_sum=False
+        if continue_sum==True:
+            my_sum+=number
+        if number==8:
+            continue_sum=True
+    return my_sum
+
+out1=sum78([1, 2, 2]) 
+out2=sum78([1, 2, 2, 7, 99, 99, 8]) 
+out3=sum78([1, 1, 7, 8, 2]) 
+print(out1,out2,out3)
