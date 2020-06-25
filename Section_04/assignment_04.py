454ea253f109f0712b1c27564b45d0826abeb1ca
michaelrizzo2 <m_rizzo@live.com>
2020-06-25 15:37:40 -0400

    Finished assignemtn 4.

--- Section_04/assignment_04.py
+++ Section_04/assignment_04.py
@@ -8,3 +8,13 @@ grow_string('abc') → 'aababc'
 grow_string('ab') → 'aab'
 
 """
+def grow_string(my_string):
+    result=''
+    for i in range(len(my_string)):
+        result+=my_string[0:i+1]
+    return result
+
+out1=grow_string('Code') 
+out2=grow_string('abc') 
+out3=grow_string('ab') 
+print(out1,out2,out3)
