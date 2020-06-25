8a6b93689c0507c7353af7abf5bd52a90d2ef0b9
michaelrizzo2 <m_rizzo@live.com>
2020-06-25 14:47:29 -0400

    Finished Assignment 3.

--- Section_04/assignment_03.py
+++ Section_04/assignment_03.py
@@ -10,3 +10,16 @@ sequence([1, 1, 2, 1, 2, 3]) → True
 sequence([1, 2]) → False
 sequence([]) → False
 """
+
+def sequence(my_list):
+    for i in range(len(my_list)-2):
+        if my_list[i]==1 and my_list[i+1]==2 and my_list[i+2]==3:
+            return True
+    return False
+
+out1=sequence([1, 1, 2, 3, 1]) 
+out2=sequence([1, 1, 2, 4, 1]) 
+out3=sequence([1, 1, 2, 1, 2, 3]) 
+out4=sequence([1, 2]) 
+out5=sequence([]) 
+print(out1,out2,out3,out4,out5)
