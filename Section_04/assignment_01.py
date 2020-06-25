c9a46d3e5d73e74c178f696d89041df9efba727b
michaelrizzo2 <m_rizzo@live.com>
2020-06-25 14:35:38 -0400

    Finished Assignment 1.

--- Section_04/assignment_01.py
+++ Section_04/assignment_01.py
@@ -11,3 +11,14 @@ twelver(4, 9) → False
 twelver(9, 3) → True
 
 """
+def twelver(a,b):
+    if a ==12 or b==12 or a+b==12:
+        return True
+    else:
+        return False
+
+out1=twelver(3, 12)
+out2=twelver(4, 9) 
+out3=twelver(9, 3) 
+
+print(out1,out2,out3)
