df29e29df3d2cc34e668fb93ed16f6dbe5bd882c
michaelrizzo2 <m_rizzo@live.com>
2020-06-24 22:15:05 -0400

    Finished the in keyword

---
+++ cool functions.py
@@ -0,0 +1,18 @@
+list_1=[1,2,3,4,5,6]
+list_2=["hi","my","Name","is","Michael","Rizzo"]
+list_3=[7,8,9,10,11,12]
+
+#We can iterate through three lists at the same time
+
+for item1,item2,item3 in zip(list_1,list_2,list_3):
+    print(item1,item2,item3)
+
+# We can probably extend this through multiple lists.
+#We can also do this
+
+for a,b,c in list(zip(list_1,list_2,list_3)):
+    print(a,b,c)
+
+print('z' in list_1)#This will return a boolean vaslue of false.
+
+print ("John" in {'John':140})#THis will return a boolean value of true
