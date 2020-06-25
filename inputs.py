26eae7961c336c91bc63b12368984baad04ff8c1
michaelrizzo2 <m_rizzo@live.com>
2020-06-25 14:20:00 -0400

    Showed how to use int iput for asking the person for a number.

--- inputs.py
+++ inputs.py
@@ -1,3 +1,5 @@
 #we can ask the user for inupt
 out=input("Please input a string")
 print(out.strip())#The strip method will remove any extra spaces
+#in python2 the default type for input was a string in python 3 it is a string, raw_input was in python2 and it changed to input in python 3
+out2=int(input("Please give me a number between 1 and 10"))
