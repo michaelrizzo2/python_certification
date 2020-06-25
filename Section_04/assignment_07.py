e56beae9b7b181f141fae1621deaf3801fbd8601
michaelrizzo2 <m_rizzo@live.com>
2020-06-25 19:11:43 -0400

    Finished Assignment 7.

--- Section_04/assignment_07.py
+++ Section_04/assignment_07.py
@@ -11,3 +11,18 @@ EXAMPLE:
     string_match('abc', 'axc') → 0
 
 """
+def string_match(string1,string2):
+    #We need to get the length of the shortest string
+    positions_matched=0
+    short_length=min(len(string1),len(string2))
+    for i in range(short_length-1):
+        string1_sub=string1[i:i+2]
+        string2_sub=string2[i:i+2]
+        if string1_sub==string2_sub:
+            positions_matched+=1
+    return positions_matched
+
+out1=string_match('xxcaazz', 'xxbaaz')
+out2=string_match('abc', 'abc')
+out3=string_match('abc', 'axc') 
+print(out1,out2,out3)
