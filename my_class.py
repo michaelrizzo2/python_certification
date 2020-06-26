9bde163527e66b14d9f24c654c58a82a37e82402
michaelrizzo2 <m_rizzo@live.com>
2020-06-26 17:08:45 -0400

    Changed a class variable.

--- my_class.py
+++ my_class.py
@@ -1,10 +1,13 @@
 class Vehicle:
+    color="Red"#This is a class variable that should not be changed.This is a global class variable
     def __init__(self,body,make):
         self.body=body
         self.make=make
-
+#we are going to show how to change this.
+Vehicle.color="green"#The red changed to green.
 my_vehicle=Vehicle("saturn","ion")
 my_other_vehicle=Vehicle("Toyota","Corolla")
 #We can create as many vehicles as we want based off of one class
 print(my_vehicle.body,my_vehicle.make)
 print(my_other_vehicle.body,my_other_vehicle.make)
+print(my_other_vehicle.color,my_vehicle.color)
