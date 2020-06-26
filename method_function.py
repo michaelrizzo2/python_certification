340af7071cbc260adbe2b70e2175578fd700dad2
michaelrizzo2 <m_rizzo@live.com>
2020-06-25 21:53:21 -0400

    Showed the difference between a method and a function.

---
+++ method_function.py
@@ -0,0 +1,9 @@
+my_list=[1,2,3,4]
+#A method can be called on an object a function cannot.
+my_list.pop()
+
+#Example of a function being used
+max_value=max(my_list)
+
+#Methods change the result and functions do not.
+#In Short, a method is a function which belongs to an object.
