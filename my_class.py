57d5a9ef162d42a69b6436341ef7e91ce4c52541
michaelrizzo2 <m_rizzo@live.com>
2020-06-26 11:39:58 -0400

    Finished Lesson

--- my_class.py
+++ my_class.py
@@ -5,5 +5,6 @@ class Vehicle:
 
 my_vehicle=Vehicle("saturn","ion")
 my_other_vehicle=Vehicle("Toyota","Corolla")
+#We can create as many vehicles as we want based off of one class
 print(my_vehicle.body,my_vehicle.make)
 print(my_other_vehicle.body,my_other_vehicle.make)
