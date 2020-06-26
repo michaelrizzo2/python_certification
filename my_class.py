9fab38b8b8af0281d19f96bd11904ba98fa16f47
michaelrizzo2 <m_rizzo@live.com>
2020-06-26 18:47:18 -0400

    Finished the difference between class variables and object variables.

--- my_class.py
+++ my_class.py
@@ -1,13 +1,26 @@
 class Vehicle:
-    color="Red"#This is a class variable that should not be changed.This is a global class variable
-    def __init__(self,body,make):
+    #color="Red"#This is a class variable that should not be changed.This is a global class variable
+    vehicle_counter=0
+    def __init__(self,body,make,color,engine):
         self.body=body
         self.make=make
+        self.color=color
+        self.engine=engine
+        Vehicle.vehicle_counter+=1
+    def get_vehicle(self):
+        print(Vehicle.vehicle_counter)
+    def get_make(self):
+        print(self.make)
+    def get_color(self):
+        print(self.color)
 #we are going to show how to change this.
-Vehicle.color="green"#The red changed to green.
-my_vehicle=Vehicle("saturn","ion")
-my_other_vehicle=Vehicle("Toyota","Corolla")
+my_vehicle=Vehicle("saturn","ion","Green","v6")
+my_vehicle.get_vehicle()
+my_vehicle.get_make()
+my_vehicle.get_color()
+#we can change the individual color for a car(not a good idea this should be done as an instance variable and not an object variable.)
+my_other_vehicle=Vehicle("Toyota","Corolla","Gray","v4")
+my_other_vehicle.get_vehicle()
+my_other_vehicle.get_make()
+my_other_vehicle.get_color()
 #We can create as many vehicles as we want based off of one class
-print(my_vehicle.body,my_vehicle.make)
-print(my_other_vehicle.body,my_other_vehicle.make)
-print(my_other_vehicle.color,my_vehicle.color)
