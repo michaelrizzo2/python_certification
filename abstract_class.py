4e8939e06929cc271ef2e83a022c54971ee1e7b5
michaelrizzo2 <m_rizzo@live.com>
2020-06-30 12:24:44 -0400

    Finshed abstract class.

--- abstract_class.py
+++ abstract_class.py
@@ -9,5 +9,15 @@ class animal:
 class monkey(animal):#The method need to be in the child class only(methods cannot be invoked in the abstract class.)
     def eat(self):
         print("Monkey eating bananas")
+
+class bird(animal):
+    def eat(self):
+        print("Bird eating seeds")
+    def fly(self):
+        print("Bird flying")
 my_animal=monkey("sdb")
 my_animal.eat()
+my_bird=bird("Pete")
+my_bird.eat()
+my_bird.fly()
+#with an abstract class we do not have to inherit the constructor for each of the child clasees. 
