a77c7bca88064861acc5178debcea16a6ef49aab
michaelrizzo2 <m_rizzo@live.com>
2020-06-30 17:36:35 -0400

    stuck on dict shortener.

--- utility_stuff.py
+++ utility_stuff.py
@@ -10,8 +10,19 @@ class listandchar(shortener):
     def print_shortened_items(self):
         print(self.original_item[0:3])
 
+class dict_shortener(shortener):
+    def print_shortened_items(self):
+        my_dict=self.original_item
+        for index in list(my_dict.keys()):
+            print(index)
+
 my_shortener=listandchar("This is a test")
 my_shortener.print_shortened_items()
+my_shortener.print_original_items()
 
 my_shortener=listandchar([1,2,3,4,5])
+my_shortener.print_shortened_items()
+my_shortener.print_original_items()
+
+my_shortener=dict_shortener({"1":1,"2":2,"3":3,"4":4})
 my_shortener.print_shortened_items()
