7fb8104267228dd38a1bffa114cea9cf241d130c
michaelrizzo2 <m_rizzo@live.com>
2020-06-30 12:03:20 -0400

    Beginning of Abstract classes.

---
+++ abstract_class.py
@@ -0,0 +1,6 @@
+#an abstract class cannot be used to create an instance of an object.
+#you can only use inheritance for an abstract class.
+
+class animal:
+    def __init__(self,name):
+        self.name=name
